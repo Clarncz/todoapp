a7ae52cc04c87b3b8615138810733851ca12c54f
unknown <PI307@csserver.local>
2024-09-23 14:44:06 +0800

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,7 @@
 <body class="h-full">
     <!--
   This example requires updating your template:
+
   ```
   <html class="h-full bg-gray-100">
   <body class="h-full">
@@ -102,7 +103,7 @@
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
-
+      
     </div>
   </header>
   <main>
